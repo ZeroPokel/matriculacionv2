a2de267c0f77ff324bf944b6a96b68d18794b776
Miguel Angel Femenia <mfemvaz2906@g.educaand.es>
2023-03-08 09:06:01 +0100

    first commit

---
+++ src/main/java/com/mafv/matriculacionv2/Matriculacionv2Application.java
@@ -0,0 +1,13 @@
+package com.mafv.matriculacionv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Matriculacionv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Matriculacionv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mafv/matriculacionv2/Matriculacionv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mafv.matriculacionv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Matriculacionv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
